9ee68ef3e467a5e2b00aaa01094ac8ed3fab2d72
Olli Etuaho <olli.etuaho@gmail.com>
2018-12-02 21:38:20 +0200

    Random rotation effect on tiles

---
+++ Assets/Scripts/lpunityutils/Scene/RandomRotationAndOffset.cs
@@ -0,0 +1,27 @@
+﻿// Copyright Olli Etuaho 2018
+
+using UnityEngine;
+
+namespace LPUnityUtils
+{
+    public class RandomRotationAndOffset : MonoBehaviour
+    {
+        public static float RandomSign
+        {
+            get
+            {
+                return Random.value > 0.5f ? 1.0f : -1.0f;
+            }
+        }
+
+        [SerializeField] private RandomRotationAndOffsetConfig Config;
+
+        void Awake()
+        {
+            transform.position += new Vector3(RandomSign * Config.XOffsetMult * Config.OffsetCurve.Evaluate(Random.value), 0.0f, RandomSign * Config.ZOffsetMult * Config.OffsetCurve.Evaluate(Random.value));
+
+            transform.rotation *= Quaternion.AngleAxis(RandomSign * Config.YRotationMult * Config.RotationExtentCurve.Evaluate(Random.value), Vector3.up);
+        }
+    }
+
+}  // namespace LPUnityUtils
