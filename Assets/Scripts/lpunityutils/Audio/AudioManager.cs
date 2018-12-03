e61ebb1466585b68478c443e0f6a9cc6a3731fe1
Olli Etuaho <olli.etuaho@gmail.com>
2018-12-03 12:45:23 +0200

    Add theme music

---
+++ Assets/Scripts/lpunityutils/Audio/AudioManager.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace LPUnityUtils
+{
+
+    public class AudioManager : Singleton<AudioManager>
+    {
+        public List<Sound> Sounds;
+
+        void Awake()
+        {
+            if (!EnforceSingleton(false))
+            {
+                return;
+            }
+
+            foreach (Sound sound in Sounds)
+            {
+                sound.Source = gameObject.AddComponent<AudioSource>();
+            }
+        }
+
+        public void Play(string soundName)
+        {
+            Sound s = Sounds.Find(sound => sound.Name == soundName);
+            if (s == null)
+            {
+                Debug.Log("Sound not found! " + soundName);
+                return;
+            }
+            s.Source.Play();
+        }
+    }
+
+}
